ae22069fe381c3734eee556e605a14ab40d7399a
Nam Nguyen <duynam.dev223@gmail.com>
2019-05-04 10:57:55 +0700

    Updates

---
+++ precache-manifest.2533a7bbefa9cd2450910e069c7115ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd81f8199347e6d628157cfcfcb1457",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "0f4a176b1bdbd6318374",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "a6340b4fdb9a0605ce14",
+    "url": "/robofriends/static/css/main.3e3a64a1.chunk.css"
+  },
+  {
+    "revision": "0f4a176b1bdbd6318374",
+    "url": "/robofriends/static/js/2.88a294b0.chunk.js"
+  },
+  {
+    "revision": "a6340b4fdb9a0605ce14",
+    "url": "/robofriends/static/js/main.820d67aa.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
